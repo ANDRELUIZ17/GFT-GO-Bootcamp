05dad08bbb8010f3978968b1c3eafd2756c9fd92
André Luiz <andlu-luz@hotmail.com>
2022-01-23 18:31:32 -0300

    First commit

---
+++ 4.Variáveis, tipos de dado e Operadores aritméticos/Exercitando1/Aula1-Conceituação e criação/src/Main.java
@@ -0,0 +1,52 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        int i;
+        //int i;
+        int I;
+        //int_1a;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10;
+        //j = 15;
+
+        int asrn24678nd;
+        //int asrn246 78nd;
+        int asrn2$4678_nd = 10;
+        //int asrn2$46%78_nd = 10;
+
+        asrn24678nd = 100;
+        asrn2$4678_nd = 10;
+
+        int quantidadeProduto = 10;
+        //int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 3;
+        //final int numero_Tentativas = 3;
+        int QUANTIDADE_OPCOES = 30;
+        //int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+        System.out.println();
+
+        System.out.println(j);
+        System.out.println(asrn24678nd);
+        System.out.println(asrn2$4678_nd);
+        System.out.println();
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+
+    }
+}
