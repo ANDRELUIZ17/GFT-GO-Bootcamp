f3c650f011444ff9e6f11e6e0a64353a4f389265
André Luiz <andlu-luz@hotmail.com>
2022-01-26 16:17:23 -0300

    Interface

---
+++ 8.Programação Orientado a Objetos/6.Interface/src/one/digitalinnovation/oo/Calculadora.java
@@ -0,0 +1,33 @@
+
+/*Apenas para praticar, crie uma interface chamada
+"OperacaoMatematica". Crie também 4 métodos das operações
+        básicas: soma, subtração, multiplicação e divisão.*/
+package one.digitalinnovation.oo;
+
+/**
+ * Classe de exemplo para o exercício da Aula 4 de Orientação a Objetos.
+ */
+class Calculadora implements OperacaoMatematica {
+
+
+    @Override
+    public void soma(double operando1, double operando2) {
+        System.out.println("Soma: " + operando1 + operando2);
+    }
+
+    @Override
+    public void subtracao(double operando1, double operando2) {
+        System.out.println("Subtracao: " + (operando1 - operando2));
+    }
+
+    @Override
+    public void multiplicacao(double operando1, double operando2) {
+        System.out.println("multiplicacao: " + operando1 * operando2);
+    }
+
+    @Override
+    public void divisao(double operando1, double operando2) {
+        System.out.println("multiplicacao: " + operando1 / operando2);
+    }
+
+}
