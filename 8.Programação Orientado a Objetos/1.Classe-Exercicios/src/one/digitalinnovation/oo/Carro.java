1875548e328c60ca991e21f6ab5c3a870ea2cb9c
André Luiz <andlu-luz@hotmail.com>
2022-01-26 14:18:28 -0300

    Programação orientado a objetos

---
+++ 8.Programação Orientado a Objetos/1.Classe-Exercicios/src/one/digitalinnovation/oo/Carro.java
@@ -0,0 +1,46 @@
+package one.digitalinnovation.oo;
+
+import com.sun.source.tree.BreakTree;
+
+public class Carro {
+    String cor;
+    String modelo;
+    int capacidadeTanque;
+
+    //
+    Carro() {
+
+    }
+
+    Carro (String cor, String modelo, int capacidadeTanque) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+
+    void setCor (String cor) {
+        this.cor = cor;
+    }
+    String getCor() {
+        return cor;
+    }
+    void setModelo (String modelo) {
+        this.modelo = modelo;
+    }
+    String getModelo () {
+        return modelo;
+    }
+    void setCapacidadeTanque (int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+
+    }
+    int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+    //método para encher o tanque
+    double totalValorTanque (double valorCombustivel) {
+        return capacidadeTanque * valorCombustivel;
+    }
+
+}
