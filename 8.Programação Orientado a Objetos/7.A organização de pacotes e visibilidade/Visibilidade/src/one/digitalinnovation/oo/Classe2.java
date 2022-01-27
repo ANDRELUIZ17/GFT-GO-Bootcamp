ed9be18603f8d77d19ef5f751357f017221ce0e3
André Luiz <andlu-luz@hotmail.com>
2022-01-27 09:24:22 -0300

    Visibilidade

---
+++ 8.Programação Orientado a Objetos/7.A organização de pacotes e visibilidade/Visibilidade/src/one/digitalinnovation/oo/Classe2.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation.oo;
+
+ class Classe2 extends Classe1{
+    //Atributos próprios
+
+     void metodo() {}
+     //atributo2 e 3
+}
+
+//metodo2 e 3
---
+++ 8.Programação Orientado a Objetos/7.A organização de pacotes e visibilidade/Visibilidade/src/one/digitalinnovation/oo/Classe3.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation.oo;
+
+ class Classe3 {
+     Classe1 classe1;
+     void metodo() {
+         //atributo2 e 3
+
+         //metodo2 e 3
+     }
+}
