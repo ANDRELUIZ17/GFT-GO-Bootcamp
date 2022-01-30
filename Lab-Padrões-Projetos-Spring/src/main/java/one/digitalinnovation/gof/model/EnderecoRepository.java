193ae2f28f63a26b8c62840cc536cfa3c6785e05
André Luiz <andlu-luz@hotmail.com>
2022-01-30 16:29:23 -0300

    Projeto Java

--- Lab-Padrões-Projetos-Spring/src/main/java/one/digitalinnovation/gof/model/EnderecoRepository.java
+++
@@ -1,8 +0,0 @@
-package one.digitalinnovation.gof.model;
-
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EnderecoRepository extends CrudRepository<Endereco, String> {
-
-}
