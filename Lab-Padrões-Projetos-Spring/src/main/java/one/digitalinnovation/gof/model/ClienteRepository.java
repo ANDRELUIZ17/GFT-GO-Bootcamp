4607d60d7071482348cd1eb2aa6de2b13ab5d76d
ANDRELUIZ17 <65025771+ANDRELUIZ17@users.noreply.github.com>
2022-01-30 15:08:00 -0300

    Delete Lab-Padrões-Projetos-Spring directory

--- Lab-Padrões-Projetos-Spring/src/main/java/one/digitalinnovation/gof/model/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package one.digitalinnovation.gof.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClienteRepository extends CrudRepository<Cliente, Long> {
-
-}
